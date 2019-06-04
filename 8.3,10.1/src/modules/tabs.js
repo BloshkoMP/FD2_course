76ad25e224596270a9720eacf064055b21535905
Bloshko Max <Maksisa@tut.by>
2019-06-04 18:53:33 +0300

    move inputTimer logic to timier.js; add validation of time input;

--- 8.3,10.1/src/modules/tabs.js
+++ 8.3,10.1/src/modules/tabs.js
@@ -1,39 +1,26 @@
 const htmlElements = {
-	links: document.querySelectorAll(".container .links a"),
-	tabs: document.querySelectorAll(".container .tabs [data-mode]"),
-	output: document.querySelector('.container .tabs [data-mode="timer"] .output'),
-	okButton: document.querySelector(".container .tabs [data-mode] .okButton"),
-	timerInput: document.querySelector(".container .tabs [data-mode] .timerOutput")
+	links: document.querySelectorAll('.container .links a'),
+	tabs: document.querySelectorAll('.container .tabs [data-mode]'),
+	output: document.querySelector('.container .tabs [data-mode="timer"] .output')
 };
-let currentTimer = "00:05:00";
 function Tab() {}
 
 Tab.prototype.initTabs = function() {
-	showSelectedTab("clock");
+	showSelectedTab('clock');
 	htmlElements.links.forEach(function(element) {
-		element.addEventListener("click", function() {
+		element.addEventListener('click', function() {
 			onLinkClicked(this);
 		});
 	});
-	htmlElements.output.addEventListener("dblclick", function() {
+	htmlElements.output.addEventListener('dblclick', function() {
 		onLinkClicked(htmlElements.tabs[3]);
 	});
-	htmlElements.okButton.addEventListener("click", onButtonClick);
 };
 
 function onLinkClicked(value) {
-	clearTimerInput();
 	switchToMode(value.dataset.mode);
 }
 
-function onButtonClick() {
-	switchToMode("timer");
-	htmlElements.output.innerText = htmlElements.timerInput.value;
-	currentTimer = htmlElements.timerInput.value;
-}
-function clearTimerInput() {
-	htmlElements.timerInput.value = "";
-}
 function switchToMode(mode) {
 	showClickedLink(mode);
 	showSelectedTab(mode);
@@ -42,9 +29,9 @@ function switchToMode(mode) {
 function showClickedLink(mode) {
 	htmlElements.links.forEach(function(element) {
 		if (element.dataset.mode === mode) {
-			element.classList.add("selected");
+			element.classList.add('selected');
 		} else {
-			element.classList.remove("selected");
+			element.classList.remove('selected');
 		}
 	});
 }
@@ -52,12 +39,12 @@ function showClickedLink(mode) {
 function showSelectedTab(mode) {
 	htmlElements.tabs.forEach(function(element) {
 		if (element.dataset.mode === mode) {
-			element.classList.remove("hidden");
+			element.classList.remove('hidden');
 		} else {
-			element.classList.add("hidden");
+			element.classList.add('hidden');
 		}
 	});
 }
 
-export { Tab };
-export { currentTimer };
+export {Tab};
+export {switchToMode};
--- 8.3,10.1/src/modules/timer.js
+++ 8.3,10.1/src/modules/timer.js
@@ -1,26 +1,35 @@
-import { currentTimer } from "./tabs.js";
+import {switchToMode} from './tabs.js';
 let hrs = 0,
 	min = 0,
 	sec = 0,
 	timerTiker;
+let currentTimer = '00:05:00';
 
 const htmlElements = {
-	output: document.querySelector(".container  [data-mode=timer] .output"),
-	start: document.querySelector(".container  [data-mode=timer] .start"),
-	stop: document.querySelector(".container  [data-mode=timer] .stop"),
-	reset: document.querySelector(".container  [data-mode=timer] .reset")
+	output: document.querySelector('.container  [data-mode=timer] .output'),
+	start: document.querySelector('.container  [data-mode=timer] .start'),
+	stop: document.querySelector('.container  [data-mode=timer] .stop'),
+	reset: document.querySelector('.container  [data-mode=timer] .reset'),
+	okButton: document.querySelector('.container .tabs [data-mode] .okButton'),
+	timerInput: document.querySelector('.container .tabs [data-mode] .timerOutput')
 };
 
 function Timer() {}
 Timer.prototype.initTimer = function() {
-	const { start, stop, reset } = htmlElements;
-	start.addEventListener("click", onStartButtonClicked);
-	stop.addEventListener("click", onStopButtonClicked);
-	reset.addEventListener("click", onResetButtonClicked);
+	const {start, stop, reset, okButton, timerInput} = htmlElements;
+	start.addEventListener('click', onStartButtonClicked);
+	stop.addEventListener('click', onStopButtonClicked);
+	reset.addEventListener('click', onResetButtonClicked);
+	okButton.addEventListener('click', onButtonClick);
+	timerInput.addEventListener('focusout', onInputFocusOut);
 };
 
+function onInputFocusOut() {
+	htmlElements.timerInput.style.borderColor = !checkIfTimeCanBeAdded(this.value) ? 'red' : 'green';
+}
+
 function onStartButtonClicked() {
-	const newTimerValue = currentTimer.split(":");
+	const newTimerValue = currentTimer.split(':');
 	hrs = parseInt(newTimerValue[0]);
 	min = parseInt(newTimerValue[1]);
 	sec = parseInt(newTimerValue[2]);
@@ -52,13 +61,72 @@ function onTimerTicker() {
 	}
 
 	htmlElements.output.innerText =
-		(hrs ? (hrs > 10 ? hrs : "0" + hrs) : "00") +
-		":" +
-		(min ? (min > 10 ? min : "0" + min) : "00") +
-		":" +
-		(sec > 10 ? sec : "0" + sec);
+		(hrs ? (hrs >= 10 ? hrs : '0' + hrs) : '00') +
+		':' +
+		(min ? (min >= 10 ? min : '0' + min) : '00') +
+		':' +
+		(sec >= 10 ? sec : '0' + sec);
 	if (!sec && !min && !hrs) {
 		clearInterval(timerTiker);
 	}
 }
-export { Timer };
+
+
+//управление временем таймера
+
+function onButtonClick() {
+	const {output, timerInput} = htmlElements;
+	if (checkIfTimeCanBeAdded(timerInput.value)) {
+		switchToMode('timer');
+		output.innerText = timerInput.value;
+		currentTimer = timerInput.value;
+		resetInput();
+	}
+}
+
+function resetInput() {
+	htmlElements.timerInput.value = '';
+	htmlElements.timerInput.style.borderColor = '';
+}
+
+function checkIfTimeCanBeAdded(input) {
+	const inputArray = input.split(':');
+	if (isInputNumber(inputArray) && isInputValid(inputArray) && isTimeCorrect(inputArray)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isInputNumber(inputArray) {
+	if (isNumber(inputArray[0]) && isNumber(inputArray[1]) && isNumber(inputArray[2])) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isTimeCorrect(inputArray) {
+	if (
+		(inputArray[0].length === 1 || inputArray[0].length === 2) &&
+		inputArray[1].length === 2 &&
+		inputArray[2].length === 2
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isInputValid(inputArray) {
+	if (parseInt(inputArray[0]) < 24 && parseInt(inputArray[1]) < 60 && parseInt(inputArray[2]) < 60) {
+		return true;
+	} else {
+		return false;
+	}
+}
+function isNumber(value) {
+	return !isNaN(value) && value.trim() !== '' && value !== null && value !== undefined;
+}
+
+export {Timer};
