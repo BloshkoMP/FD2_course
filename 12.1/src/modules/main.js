6286e58c877ee6346f3d66f14b087371ffd8fb21
Maksim Bloshko <maks.bloshko@gmail.com>
2019-06-11 22:41:28 +0300

    add #12.1

---
+++ 12.1/src/modules/main.js
@@ -0,0 +1,37 @@
+﻿const obj = {
+	size: 123,
+	coords: {
+		x: 1,
+		y: {
+			value: 111,
+			direction: "top"
+		},
+		showDirection() {
+			console.log(`coords: ${this.y.direction}`);
+		}
+	},
+	showSize() {
+		console.log(`size: ${this.size}`);
+	}
+};
+
+let arr = [];
+
+function cloneObject(obj) {
+	if (arr.indexOf(obj) > -1) {
+		return "circular referense";
+	}
+	arr.push(obj);
+	let cloneObj = {};
+	for (let key in obj) {
+		if (typeof obj[key] === "object") {
+			cloneObj[key] = cloneObject(obj[key]);
+		} else {
+			cloneObj[key] = obj[key];
+		}
+	}
+	return cloneObj;
+}
+
+const getObj = cloneObject(obj);
+console.log(getObj);
