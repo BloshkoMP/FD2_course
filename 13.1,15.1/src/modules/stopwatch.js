9011c209e3cd6b5d13d7c9e1a31c2dfd9a54bcfa
Bloshko Max <Maksisa@tut.by>
2019-06-22 17:31:08 +0300

    update files after merge

--- 13.1,15.1/src/modules/stopwatch.js
+++ 13.1,15.1/src/modules/stopwatch.js
@@ -1,11 +1,7 @@
 import {StopWatchTimer} from './stopwatchTimer.js';
 
 function Stopwatch() {
-<<<<<<< HEAD:13.1,15.1/src/modules/stopwatch.js
 	StopWatchTimer.apply(this, ['stopwatch', 0]);
-=======
-	StopWatchTimer.apply(this, ["stopwatch", 0]);
->>>>>>> 1be1a958998fe190f1e7b21ffbdeb9825211c42f:13.1,15.1/src/modules/stopwatch.js
 }
 
 Stopwatch.prototype = Object.create(StopWatchTimer.prototype);
