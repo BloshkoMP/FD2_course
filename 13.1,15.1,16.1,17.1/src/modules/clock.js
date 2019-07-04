53131fdcc5430bf89b58d447a1d1b765bde3ae16
Maksim Bloshko <maks.bloshko@gmail.com>
2019-07-05 00:19:23 +0300

    update links in root index.html, minor fix

--- 13.1,15.1,16.1,17.1/src/modules/clock.js
+++ 13.1,15.1,16.1,17.1/src/modules/clock.js
@@ -5,11 +5,16 @@ function Clock() {}
 
 Clock.prototype.initClock = function() {
 	onIntervalTicker();
-	setInterval(onIntervalTicker, 1000);
+	setTimeout(() => {
+		onIntervalTicker();
+	}, 1000);
 };
 function onIntervalTicker() {
 	const currentDate = new Date();
 	const currentTime = currentDate.toTimeString().split(" ")[0];
 	htmlElements.output.innerText = currentTime;
+	setTimeout(() => {
+		onIntervalTicker();
+	}, 1000);
 }
 export { Clock };
