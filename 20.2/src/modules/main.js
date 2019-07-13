78fe275030fc0ecb8d3879017f08a7f89469eb3c
Maksim Bloshko <maks.bloshko@gmail.com>
2019-07-13 14:53:19 +0300

    add: final project TETRIS link, move TETRIS in new folder

---
+++ 20.2/src/modules/main.js
@@ -0,0 +1,17 @@
+﻿window.plus = () => {
+	let elem = document.querySelector("form");
+	elem[2].value = parseInt(elem[0].value) + parseInt(elem[1].value);
+};
+
+window.subtract = () => {
+	let elem = document.querySelector("form");
+	elem[2].value = parseInt(elem[0].value) - parseInt(elem[1].value);
+};
+window.multiply = () => {
+	let elem = document.querySelector("form");
+	elem[2].value = parseInt(elem[0].value) * parseInt(elem[1].value);
+};
+window.divide = () => {
+	let elem = document.querySelector("form");
+	elem[2].value = parseInt(elem[0].value) / parseInt(elem[1].value);
+};
